de8c3cc11cbeed4dee4d363079f26eb12797b187
Tien Eu Min <tieneumin@gmail.com>
2025-05-02 11:50:49 +0800

    Uncommented MainActivity redirect logic and changed start destination

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -18,19 +18,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val navHostFragment = supportFragmentManager
-//            .findFragmentById(R.id.navHostFragment) as NavHostFragment
-//        val navController = navHostFragment.navController
-//        val navGraph = navController.navInflater.inflate(R.navigation.nav_graph)
-//
-//        lifecycleScope.launch {
-//            val startDestination = when (authService.getUserRole()) {
-//                "student" -> R.id.homeStudentFragment
-//                "teacher" -> R.id.homeTeacherFragment
-//                else -> R.id.loginFragment
-//            }
-//            navGraph.setStartDestination(startDestination)
-//            navController.setGraph(navGraph, null)
-//        }
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navGraph = navController.navInflater.inflate(R.navigation.nav_graph)
+
+        lifecycleScope.launch {
+            val startDestination = when (authService.getUserRole()) {
+                "student" -> R.id.homeStudentFragment
+                "teacher" -> R.id.homeTeacherFragment
+                else -> R.id.loginFragment
+            }
+            navGraph.setStartDestination(startDestination)
+            navController.setGraph(navGraph, null)
+        }
     }
 }
